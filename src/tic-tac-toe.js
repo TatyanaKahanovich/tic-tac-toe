876b3525c68f30f90ce812d6b1a27f4723b6ff52
Tatyana Kahanovich <d-m-tanya@yandex.ru>
2017-09-27 18:12:32 +0300

    intermediate result

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,13 +1,19 @@
 class TicTacToe {
     constructor() {
-
+    this.simbol = "x";
+    this.arr = [[null, null, null], [null, null, null], [null, null, null]];
+    this.winner = false;
     }
 
     getCurrentPlayerSymbol() {
-
+        return this.simbol;
     }
 
     nextTurn(rowIndex, columnIndex) {
+		if (this.arr[rowIndex][columnIndex] === null){
+			this.arr[rowIndex][columnIndex] = this.simbol;
+			this.simbol === "x" ? this.simbol = "o" : this.simbol = "x";
+		}
 
     }
 
@@ -16,19 +22,62 @@ class TicTacToe {
     }
 
     getWinner() {
+		let a;
+		this.simbol === "x" ?  a = "o" :  a = "x";
+		this.winner = true;
+        for (let i = 0; i < 3; i++) {
+			if (a === this.arr[i][0]
+            && a === this.arr[i][1]
+            && a === this.arr[i][2]) {
+                return a;
+            }
 
+			if (a === this.arr[0][i]
+            && a === this.arr[1][i]
+            && a === this.arr[2][i]) {
+                return a;
+			}
+
+			if (a === this.arr[0][0]
+            && a === this.arr[1][1]
+            && a === this.arr[2][2]) {
+                return a;
+			}
+
+			if (a === this.arr[0][2]
+            && a === this.arr[1][1]
+            && a === this.arr[2][0]) {
+                return a;
+			}
+        }
+        this.winner = false;
+        return null;
     }
 
     noMoreTurns() {
-
+        for (let i = 0; i < 3; i++){
+            for (let j = 0; j < 3; j++){
+                if(this.arr[i][j] === null)    {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     isDraw() {
-
+        for (let i = 0; i < 3; i++){
+            for (let j = 0; j < 3; j++){
+                if(this.arr[i][j] !== null && this.winner === false)    {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+        return this.arr[rowIndex][colIndex];
     }
 }
 
